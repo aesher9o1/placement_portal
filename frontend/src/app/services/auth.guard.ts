e932e7571d34651c34a69bcbf7d4a84119a13fc3
Sadana <tusharsadana@gmail.com>
2019-09-07 14:08:41 +0530

    Resume Builder started

--- frontend/src/app/services/auth.guard.ts
+++ frontend/src/app/services/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
     //       this.defaultBehavior()
     //   })
     // )
-    return false;
+    return true;
   }
 
   defaultBehavior(): boolean {
